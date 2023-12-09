20b2db9708590037d91924e460b06d82ffbc93a9
Karson Daecher <kbd45@cornell.edu>
2023-12-09 15:18:21 -0800

    integrate vercel web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter, Inria_Serif } from 'next/font/google'
 import localFont from 'next/font/local'
+import { Analytics } from '@vercel/analytics/react';
 import SiteTitle from './components/SiteTitle';
 
 const inter = Inter({
@@ -45,6 +46,7 @@ export default function RootLayout({
             {children}
           </div>
         </div>
+        <Analytics />
       </body>
     </html>
   );
