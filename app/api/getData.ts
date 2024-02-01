b8f9b5aa029dbe9afd6cd9bbaa2736da695b7188
Karson Daecher <kbd45@cornell.edu>
2024-01-31 21:08:22 -0500

    cache no store & update .gitignore

--- app/api/getData.ts
+++ app/api/getData.ts
@@ -4,7 +4,7 @@ import ScrapeMap from './ScrapeMap';
 
 export default async function getData(): Promise<Record<string, Showtime[]>> {
   const showtimes = (await Promise.all(ScrapeMap.map(async ({ url, parser }) => {
-    const response = await fetch(url, { next: { revalidate: 3600 } });
+    const response = await fetch(url, { cache: 'no-store' });
     const html = await response.text();
     const showtimes = parser(html);
     return showtimes;
