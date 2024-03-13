81ff1ac6833e3617c2ea21e5687174d65ee6c176
Karson Daecher <kbd45@cornell.edu>
2024-03-13 09:05:21 -0400

    update cron job time + don't cache results

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,14 @@ import { createClient } from '@supabase/supabase-js';
 import Home from './components/Home';
 import groupBy from './utils/groupBy';
 
+export const revalidate = 0;
+
 export default async function Root() {
   const supabaseUrl = process.env.SUPABASE_URL;
   const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
   if (!supabaseUrl || !supabaseKey) {
+    console.error('Env variables not found');
     throw new Error('Env variables not found');
   }
 
@@ -19,10 +22,12 @@ export default async function Root() {
     .select('*');
 
   if (error) {
+    console.error(error.message);
     throw error;
   }
 
   if (!showtimes) {
+    console.error(`No showtimes found`);
     throw new Error('No showtimes found');
   }
         
