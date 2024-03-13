2cb7945deef52580b216fe8ec9de691ea65bc428
Karson Daecher <kbd45@cornell.edu>
2024-03-12 20:30:41 -0400

    remove NEXT_PUBLIC env vars

--- app/api/scrape/route.ts
+++ app/api/scrape/route.ts
@@ -3,11 +3,17 @@ import { NextResponse } from 'next/server';
 
 import getData from '../getData';
 
+const __DEV__ = process.env.NODE_ENV === 'development';
+
 export const dynamic = 'force-dynamic'; 
 
 export async function GET() {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  const supabaseUrl = __DEV__ ?
+    process.env.NEXT_PUBLIC_SUPABASE_URL :
+    process.env.SUPABASE_URL;
+  const supabaseKey = __DEV__ ?
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY :
+    process.env.SUPABASE_ANON_KEY;
 
   if (!supabaseUrl || !supabaseKey) {
     return NextResponse.json({ error: 'Env variables not found' }, { status: 500 });
