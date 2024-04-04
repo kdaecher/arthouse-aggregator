35953fb169edd10bc399cfe0e40d2bd93df48997
Karson Daecher <kbd45@cornell.edu>
2024-04-04 11:49:38 -0400

    scrape Quad cinema and add api for single theater scraping

--- app/api/ScrapeMap.ts
+++ app/api/ScrapeMap.ts
@@ -1,26 +1,37 @@
+import Theater from '../types/Theater';
 import * as parsers from './parsers';
 
 const map = [
   {
+    theater: Theater.METROGRAPH,
     url: 'https://metrograph.com/calendar',
     parser: parsers.metrograph,
   },
   {
+    theater: Theater.FILM_FORUM,
     url: 'https://filmforum.org/now_playing',
     parser: parsers.filmForum,
   },
   {
+    theater: Theater.ANTHOLOGY_FILM_ARCHIVES,
     url: 'http://anthologyfilmarchives.org/film_screenings/calendar',
     parser: parsers.anthology,
   },
   {
+    theater: Theater.IFC_CENTER,
     url: 'https://www.ifccenter.com/',
     parser: parsers.ifcCenter,
   },
   {
+    theater: Theater.CINEMA_VILLAGE,
     url: 'https://www.cinemavillage.com/calendar/',
     parser: parsers.cinemaVillage,
-  }
+  },
+  {
+    theater: Theater.QUAD_CINEMA,
+    url: 'https://quadcinema.com/all',
+    parser: parsers.quadCinema,
+  },
 ];
 
 export default map;
--- app/api/parsers/index.ts
+++ app/api/parsers/index.ts
@@ -3,3 +3,4 @@ export { default as filmForum } from './filmForum';
 export { default as anthology } from './anthology';
 export { default as ifcCenter } from './ifcCenter';
 export { default as cinemaVillage } from './cinemaVillage';
+export { default as quadCinema } from './quadCinema';
